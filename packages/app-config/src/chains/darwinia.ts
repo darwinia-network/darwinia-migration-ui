8cf249e82f1b461b265dec79e875160375aa2217
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2023-04-19 19:01:16 +0800

    Added Darwinia network

--- packages/app-config/src/chains/darwinia.ts
+++ packages/app-config/src/chains/darwinia.ts
@@ -5,7 +5,7 @@ export const darwinia: ChainConfig = {
   name: "Darwinia",
   displayName: "Darwinia",
   explorerURLs: ["https://darwinia.subscan.io/"],
-  httpsURLs: ["https://rpc.darwinia.network"],
+  httpsURLs: ["https://cors.kahub.in/http://g1.dev.darwinia.network:10000"],
   kton: {
     address: "0x0000000000000000000000000000000000000402",
     symbol: "KTON",
@@ -27,7 +27,20 @@ export const darwinia: ChainConfig = {
   chainId: 46,
   prefix: 18,
   substrate: {
-    graphQlURL: "https://subql.darwinia.network/subql-apps-darwinia/",
-    wssURL: "wss://rpc.darwinia.network",
+    graphQlURL: "https://api.subquery.network/sq/isunaslabs/darwinia2",
+    wssURL: "ws://g1.dev.darwinia.network:20000",
   },
 };
+
+/*
+ * DevNet
+ * httpsURLs: ["https://cors.kahub.in/http://g1.dev.darwinia.network:10000"]
+ * graphQlURL: "https://api.subquery.network/sq/isunaslabs/darwinia2",
+ * wssURL: "ws://g1.dev.darwinia.network:20000",
+ *
+ * LiveNet
+ * httpURLs: ["https://rpc.darwinia.network"]
+ * wssURL: "wss://rpc.darwinia.network",
+ * graphQlURL: "https://subql.darwinia.network/subql-apps-darwinia/",
+ *
+ * */
