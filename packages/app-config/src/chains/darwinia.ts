8ada65b0c6421c9ed2988d695e9ad8514a35be9c
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2023-04-24 18:24:13 +0800

    changed the Darwinia configurations from dev to live

--- packages/app-config/src/chains/darwinia.ts
+++ packages/app-config/src/chains/darwinia.ts
@@ -5,7 +5,7 @@ export const darwinia: ChainConfig = {
   name: "Darwinia",
   displayName: "Darwinia",
   explorerURLs: ["https://darwinia.subscan.io/"],
-  httpsURLs: ["https://cors.zimjs.com/http://g1.dev.darwinia.network:10000"],
+  httpsURLs: ["https://rpc.darwinia.network"],
   kton: {
     address: "0x0000000000000000000000000000000000000402",
     symbol: "KTON",
@@ -27,8 +27,8 @@ export const darwinia: ChainConfig = {
   chainId: 46,
   prefix: 18,
   substrate: {
-    graphQlURL: "https://api.subquery.network/sq/isunaslabs/darwinia2",
-    wssURL: "ws://g1.dev.darwinia.network:20000",
+    graphQlURL: "https://subql.darwinia.network/subql-apps-darwinia/",
+    wssURL: "wss://rpc.darwinia.network",
   },
 };
 
